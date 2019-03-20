380a4250e00a65a8ca039f8fb9428c32b1ec94bf
EC2 Default User <ec2-user@ip-172-31-43-32.us-east-2.compute.internal>
2019-03-20 22:16:21 +0000

    Added serving input fn

--- S3_cps.sh
+++ S3_cps.sh
@@ -1,4 +1,7 @@
 aws s3 cp --recursive s3://car-classifier-us-east-2/python_readable_data/stanford_cars_dataset/convnet_model/ ~/convnet_model/
+
 aws s3 cp s3://car-classifier-us-east-2/python_readable_data/stanford_cars_dataset/scaled_bounded_grayscale_dict.p ~ 
+
 aws s3 cp s3://car-classifier-us-east-2/python_readable_data/stanford_cars_dataset/stanford_labels_cleaned.csv ~
+
 aws s3 cp s3://car-classifier-us-east-2/python_readable_data/stanford_cars_dataset/training_data_meta.csv ~
