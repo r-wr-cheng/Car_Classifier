4e961b5068db7da71b372729e0cc2e00ca0b7be2
EC2 Default User <ec2-user@ip-172-31-38-126.us-east-2.compute.internal>
2019-03-21 05:18:03 +0000

    Traning

--- S3_cps.sh
+++ S3_cps.sh
@@ -1,4 +1,4 @@
-aws s3 cp --recursive s3://car-classifier-us-east-2/python_readable_data/stanford_cars_dataset/convnet_model/ ~/convnet_model/
+#aws s3 cp --recursive s3://car-classifier-us-east-2/python_readable_data/stanford_cars_dataset/convnet_model/ ~/convnet_model/
 
 aws s3 cp s3://car-classifier-us-east-2/python_readable_data/stanford_cars_dataset/scaled_bounded_grayscale_dict.p ~ 
 
